9e94cfcf1ec7806bc639c2ed812d7e7089bc157e
venkatram64 <venkat.veerareddy@hotmail.com>
2021-03-20 21:19:28 +0530

    init commit

---
+++ src/main/java/com/venkat/entity/City.java
@@ -0,0 +1,49 @@
+package com.venkat.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private Integer id;
+    private String city;
+    @Column(name = "country_id")
+    private Integer countryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Integer countryId) {
+        this.countryId = countryId;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", countryId=" + countryId +
+                '}';
+    }
+}
